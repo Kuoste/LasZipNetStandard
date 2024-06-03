d05c08140142eb3e5472c9fed4e21d0fb652cd6d
Vellu Sorvari <vepeli@gmail.com>
2024-06-03 12:35:49 +0300

    Adding CC 4.0 attributions

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -56,6 +56,10 @@ namespace SampleProject
             }
 
             lasZip.CloseReader();
+
+            Console.Write(Environment.NewLine + Environment.NewLine + "Attributions:" + Environment.NewLine);
+            Console.WriteLine($"Sample file contains open data from NLS Finland, lisenced under https://creativecommons.org/licenses/by/4.0/deed.en");
+            Console.WriteLine($"Sample file contains point cloud data from file Q5232G1.laz. Accessed May 2023. Data is cropped for smaller file size.");
         }
 
         static void PrintCoordinates(LasPoint p)
